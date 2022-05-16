65d4834312e2d19ee09bcaab32b93f389c8098a0
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-16 14:41:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*Greater Than Operator: Checking if a value is > 100 */
+
+function testGreaterThan(valueTested)
+{
+    if (valueTested > 100) 
+    	 {
+            return "The value tested is greater than 100.";
+         }
+         return "Libidibidi";
+}
+
+console.log(testGreaterThan(199));
