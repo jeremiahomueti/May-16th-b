3fedd211a1306e7efb31452b149e3980c2e3d4d2
jeremiahomueti <jeremiahomueti@gmail.com>
2022-05-16 16:10:55 +0100

    Update

--- index1.js
+++ index1.js
@@ -2,11 +2,23 @@
 
 function testGreaterThan(valueTested)
 {
+    if (valueTested > 0 && valueTested < 11)
+        {
+            return "The value tested is greater than 1.";
+        }
+ 
+    if (valueTested > 9 && valueTested < 100) 
+        {
+           return "The value tested is greater than 10.";
+        }
+
     if (valueTested > 100) 
     	 {
             return "The value tested is greater than 100.";
          }
-         return "Libidibidi";
+
+    return "The value is either gibberish, a negative number or zero";
+        
 }
 
-console.log(testGreaterThan(199));
+console.log(testGreaterThan(10000));
